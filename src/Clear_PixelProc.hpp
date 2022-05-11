b1ad8988aac5b02b9145950428f19bf00de79fdc
Steph OHara <soh_la@yahoo.com>
2022-05-11 13:45:51 +1000

    fooling around with reseting

---
+++ src/Clear_PixelProc.hpp
@@ -0,0 +1,13 @@
+//
+//  Clear_PixelProc.hpp
+//  KinectExplorer
+//
+//  Created by Stephen OHara on 10/5/22.
+//
+
+#ifndef Clear_PixelProc_hpp
+#define Clear_PixelProc_hpp
+
+#include <stdio.h>
+
+#endif /* Clear_PixelProc_hpp */
